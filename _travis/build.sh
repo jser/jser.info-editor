9b66e15a5efb49f2a0e3f806eabfe56260aee4f7
azu <azuciao@gmail.com>
2014-10-13 00:36:40 +0900

    remove linux build

--- _travis/build.sh
+++ _travis/build.sh
@@ -7,8 +7,8 @@ if [[ "$TRAVIS_TAG" ]]; then
     # zipped in build/
     zip -q JSer.info-editor-osx.zip -r jser.info-editor/osx
     zip -q JSer.info-editor-win.zip -r jser.info-editor/win
-    zip -q JSer.info-editor-linux32.zip -r jser.info-editor/linux32
-    zip -q JSer.info-editor-linux64.zip -r jser.info-editor/linux64
+#    zip -q JSer.info-editor-linux32.zip -r jser.info-editor/linux32
+#    zip -q JSer.info-editor-linux64.zip -r jser.info-editor/linux64
     cd ../
     echo "zipped!"
 else
