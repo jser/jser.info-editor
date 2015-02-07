4effc49366425697fd43258355210b7a133ffcca
azu <azuciao@gmail.com>
2015-02-07 18:44:57 +0900

    Release 1.0.14

--- _travis/build.sh
+++ _travis/build.sh
@@ -6,7 +6,8 @@ if [[ "$TRAVIS_TAG" ]]; then
     cd build
     # zipped in build/
     zip -q JSer.info-editor-osx.zip -r jser.info-editor/osx64
-    zip -q JSer.info-editor-win.zip -r jser.info-editor/win
+    zip -q JSer.info-editor-win32.zip -r jser.info-editor/win32
+    zip -q JSer.info-editor-win64.zip -r jser.info-editor/win64
 #    zip -q JSer.info-editor-linux32.zip -r jser.info-editor/linux32
 #    zip -q JSer.info-editor-linux64.zip -r jser.info-editor/linux64
     cd ../
